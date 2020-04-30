f722df2ac7623963d6709b161a4fba855b120660
Linkon Siddique <lsiddique415@gmail.com>
2020-05-01 05:40:19 +0600

    Adding meal model and dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      //home: CategoriesScreen(),
       routes: {
         '/': (ctx) => CategoriesScreen(),
         '/category-meals': (ctx) => CategoryMealsScreen(),
