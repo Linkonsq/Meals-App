b1b9e7893a63156724808983eecb41c85662f069
Linkon Siddique <lsiddique415@gmail.com>
2020-05-02 06:14:20 +0600

    Finishing the meal list item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,30 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    if (complexity == Complexity.Simple) {
+      return 'Simple';
+    }
+    if (complexity == Complexity.Challenging) {
+      return 'Challenging';
+    }
+    if (complexity == Complexity.Hard) {
+      return 'Hard';
+    }
+  }
+
+  String get affordabilityText {
+    if (affordability == Affordability.Affordable) {
+      return 'Affordable';
+    }
+    if (affordability == Affordability.Pricey) {
+      return 'Pricey';
+    }
+    if (affordability == Affordability.Luxurious) {
+      return 'Expensive';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -44,7 +68,54 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
